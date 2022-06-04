afe4520e5ecc63ddd32a043a0e4b3ece60c55af8
vanibm15 <vanibm15@gmail.com>
2022-06-04 22:39:26 +0530

    added:firstname

--- src/main/java/com/bridgelabz/UserRegistration.java
+++ src/main/java/com/bridgelabz/UserRegistration.java
@@ -5,17 +5,14 @@ import java.util.regex.Pattern;
 
 public class UserRegistration {
 
-    public static boolean passwordValidation(String email) {
-        String pattern = "([a-zA-Z0-9.-]{3,})[a-zA-Z0-9.-][@][a-zA-Z0-9.-_][a-zA-Z0-9.-_]{3,}[a-zA-Z0-9.-_]*";
-        Pattern pass = Pattern.compile(pattern);
-        Matcher match = pass.matcher(email);
-        if (match.matches()) {
-            return true;
-        }
-        return false;
-    }
+    Pattern pattern;
+    Matcher matcher;
+    public static final String NAME_PATTERN = "^[A-Z][a-z]{2,}$";
 
-    public static void main(String[] args) {
-        System.out.println(passwordValidation("amolnagose12@gmail.com"));
+    public boolean validateFirstName(String firstName) {
+        Pattern pattern = Pattern.compile(NAME_PATTERN);
+        Matcher matcher = pattern.matcher(firstName);
+        return matcher.matches();
     }
 }
+
--- src/test/java/com/bridgelabz/UserRegistraionTest.java
+++ src/test/java/com/bridgelabz/UserRegistraionTest.java
@@ -1,5 +1,6 @@
 package com.bridgelabz;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.regex.Matcher;
@@ -8,54 +9,9 @@ import java.util.regex.Pattern;
 import static org.junit.jupiter.api.Assertions.assertSame;
 
 class UserRegistraionTest {
-    public class UserRegistrationTest {
-        @Test
-        public void checkValidEmail() {
-            UserRegistration user = new UserRegistration();
-            boolean pass = user.passwordValidation("abc.zxy@yahoo.com.in");
-            assertSame(true, pass);
-        }
-
-        @Test
-        public void checkValidEmail2() {
-            UserRegistration user = new UserRegistration();
-            boolean pass = user.passwordValidation("abc-100@yahoo.com");
-            assertSame(true, pass);
-        }
-
-        @Test
-        public void checkValidEmail3() {
-            UserRegistration user = new UserRegistration();
-            boolean pass = user.passwordValidation("abc.100@yahoo.com");
-            assertSame(true, pass);
-        }
-
-        @Test
-        public void checkValidEmail4() {
-            UserRegistration user = new UserRegistration();
-            boolean pass = user.passwordValidation("abc111@abc.com");
-            assertSame(true, pass);
-        }
-
-        @Test
-        public void checkValidEmail5() {
-            UserRegistration user = new UserRegistration();
-            boolean pass = user.passwordValidation("abc-100@yahoo.net");
-            assertSame(true, pass);
-        }
-
-        @Test
-        public void checkValidEmail6() {
-            UserRegistration user = new UserRegistration();
-            boolean pass = user.passwordValidation("abc.100@abc.com.au");
-            assertSame(true, pass);
-        }
-
-        @Test
-        public void checkValidEmail7() {
-            UserRegistration user = new UserRegistration();
-            boolean pass = user.passwordValidation("abc@gmail.com.com");
-            assertSame(true, pass);
-        }
+    public void givenFirstName_WhenProper_ShouldReturnTrue() {
+        UserRegistration userRegistration = new UserRegistration();
+        boolean result = userRegistration.validateFirstName("Vani");
+        Assertions.assertTrue(result);
     }
 }
